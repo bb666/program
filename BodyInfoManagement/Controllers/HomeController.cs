34c4c7118f0edb9fe80b617df2e395eddef86691
bb666 <wang416454058@qq.com>
2016-11-09 18:53:00 +0800

    third

---
+++ BodyInfoManagement/Controllers/HomeController.cs
@@ -0,0 +1,180 @@
+﻿using BodyInfoManagement.Models;
+using BodyInfoManagement.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BodyInfoManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BodyinfoModel db = new BodyinfoModel();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Administrators()
+        {
+            return View();
+        }
+
+        public ActionResult Teachers()
+        {
+            return View();
+        }
+
+        public ActionResult Students()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> BackTeachersAuth()
+        {
+            var todo = db.Teachers.Where(s => s.StudentAuth);
+            foreach (var item in todo)
+            {
+                item.StudentAuth = false;
+            }
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Teachers");
+        }
+
+        public async Task<ActionResult> SetTeachersAuth()
+        {
+            var todo = db.Teachers.Where(s => !s.StudentAuth);
+            foreach (var item in todo)
+            {
+                item.StudentAuth = true;
+            }
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Teachers");
+        }
+
+        public async Task<ActionResult> BackStudentsAuth()
+        {
+            var todo = db.Students.Where(s => s.StudentAuth);
+            foreach (var item in todo)
+            {
+                item.StudentAuth = false;
+            }
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Students");
+        }
+        public async Task<ActionResult> SetStudentsAuth()
+        {
+            var todo = db.Students.Where(s => !s.StudentAuth);
+            foreach(var item in todo)
+            {
+                item.StudentAuth = true;
+            }
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Students");
+        }
+        public static string EncipherSha(string sourceText)
+        {
+            SHA256 sha256 = new SHA256CryptoServiceProvider();
+            byte[] source = Encoding.Default.GetBytes(sourceText);
+            return Convert.ToBase64String(sha256.ComputeHash(source));
+        }
+
+        public void WriteSession(int type, string id, bool auth)
+        {
+            Session["NowUserId"] = id;
+            Session["NowUserType"] = type;
+            Session["NowUserAuth"] = auth;
+        }
+
+        public ActionResult Logoff()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Login")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel loginBag)
+        {
+            switch ((int)loginBag.LoginType)
+            {
+                case 0:
+                    {
+                        var find = await db.Teachers.FindAsync(loginBag.LoginAccount);
+                        if (find == null)
+                        {
+                            ModelState.AddModelError("LoginAccount", "用户不存在");
+                            return View(loginBag);
+                        }
+                        if (find.TeacherPassword == EncipherSha(find.SaltCode + loginBag.LoginPassword))
+                        {
+                            WriteSession(0, find.TeacherId, find.StudentAuth);
+                            return RedirectToAction("Teachers");
+                        }
+                        ModelState.AddModelError("LoginPassword", "密码错误");
+                        return View(loginBag);
+                    }
+                case 1:
+                    {
+                        var find = await db.Students.FindAsync(loginBag.LoginAccount);
+                        if (find == null)
+                        {
+                            ModelState.AddModelError("LoginAccount", "用户不存在");
+                            return View(loginBag);
+                        }
+                        if (find.StudentPassword == EncipherSha(find.SaltCode + loginBag.LoginPassword))
+                        {
+                            WriteSession(0, find.StudentId, find.StudentAuth);
+                            return RedirectToAction("Students");
+                        }
+                        ModelState.AddModelError("LoginPassword", "密码错误");
+                        return View(loginBag);
+                    }
+                case 2:
+                    {
+                        var find = await db.Administrators.FindAsync(loginBag.LoginAccount);
+                        if (find != null)
+                        {
+                            if (find.AdministratorPassword == loginBag.LoginPassword)
+                            {
+                                WriteSession(0, find.AdministratorId, true);
+                                return RedirectToAction("Administrators");
+                            }
+                        }
+
+                        ModelState.AddModelError("", "用户名或密码错误");
+                        return View(loginBag);
+                    }
+                default: break;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BodyInfoManagement/Controllers/StudentsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BodyInfoManagement.Models;
+
+namespace BodyInfoManagement.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private BodyinfoModel db = new BodyinfoModel();
+
+        // GET: Students
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Students.ToListAsync());
+        }
+
+        // GET: Students/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = await db.Students.FindAsync(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "StudentId,StudentGender,SaltCode,StudentPassword,ConfirmPassword,StudentEmail")] Student student)
+        {
+            if (student.StudentPassword != student.ConfirmPassword)
+            {
+                ModelState.AddModelError("ConfirmPassword", "密码和确认密码不匹配");
+            }
+
+            student.StudentAuth = false;
+
+            if (ModelState.IsValid)
+            {
+                student.SaltCode = Guid.NewGuid().ToString("N");
+                student.StudentPassword = HomeController.EncipherSha(student.SaltCode + student.StudentPassword);
+                db.Students.Add(student);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = await db.Students.FindAsync(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "StudentId,StudentGender,SaltCode,StudentPassword,StudentEmail,StudentAuth")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = await db.Students.FindAsync(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Student student = await db.Students.FindAsync(id);
+            db.Students.Remove(student);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
